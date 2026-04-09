88262342502df41b6b9409cfe337f9b5017205d0
hostens <stefanhosten@hotmail.com>
2026-04-09 14:48:27 +0200

    Update dcv.py

--- dcv.py
+++ dcv.py
@@ -1,1 +1,10 @@
-print("Hello World!!!")
+import sqlite3
+
+def get_user(user_name):
+  conn = sqlite3.connect("users.db")
+  cursor = conn.cursor()
+
+  query = f"SELECT * FROM users WHERE username = '{user_name}'"
+  cursor.execute(query);
+
+  return cursor.fetchAll()
