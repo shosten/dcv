e3447e8b6d028867c446dbfa9c64eef91903867b
hostens <stefanhosten@hotmail.com>
2026-04-09 15:04:26 +0200

    Update dcv.py

--- dcv.py
+++ dcv.py
@@ -1,9 +1,7 @@
 import subprocess
 
-def run(user_input):
-    # CodeQL usually flags this as command injection
-    subprocess.run("echo " + user_input, shell=True)
+def dangerous():
+    user_input = input("cmd: ")
+    subprocess.Popen(user_input, shell=True)
 
-if __name__ == "__main__":
-    data = input("Enter something: ")
-    run(data)
+dangerous()
